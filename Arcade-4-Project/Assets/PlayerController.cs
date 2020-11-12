cd412dba06a67a7461e2cbbae9e0aae50e93c800
Zephyrse <34012606+Zephyrse@users.noreply.github.com>
2020-11-12 01:26:18 +0000

    Small changes

--- Arcade-4-Project/Assets/PlayerController.cs
+++ Arcade-4-Project/Assets/PlayerController.cs
@@ -9,12 +9,6 @@ public class PlayerController : MonoBehaviour
     public float    moveSpeed = 3f;
     public Vector3  movement;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
-    // Update is called once per frame
     void Update()
     {
         Jump();
