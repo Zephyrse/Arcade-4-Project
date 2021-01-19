3cb7312d935eaac3b4836f7b6f84894c5afcd2b3
thanhtungng <up878141@myport.ac.uk>
2021-01-19 18:52:23 +0700

    scoreboard

---
+++ Arcade-4-Project/Assets/Scripts/Score.cs
@@ -0,0 +1,27 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+
+    public Transform player;
+    public Text scoreText;
+    public Text scoreMovement;
+    public Text scoreEnemies;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        
+        scoreMovement.text = ((player.position.x+10)*10).ToString("0");
+        scoreEnemies.text = 0.ToString("0");
+        //scoreText.text = scoreMovement.text.ToInt + scoreEnemies.text.ToInt;
+    }
+}
