a703bca504ff2f0f93a0e374a419b0f0b3baa6fb
thanhtungng <up878141@myport.ac.uk>
2021-01-22 23:08:56 +0700

    asd

---
+++ Arcade-4-Project/Assets/Scripts/ProgReset.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Reset() //reset highscore function
+    {
+        PlayerPrefs.DeleteKey("ProgMem");
+    }
+}
