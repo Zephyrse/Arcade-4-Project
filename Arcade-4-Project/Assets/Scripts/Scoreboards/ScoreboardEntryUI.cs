53a01dae2b75dacb601aa0336be34838597c4b88
Zephyrse <34012606+Zephyrse@users.noreply.github.com>
2021-05-14 13:06:29 +0100

    ForLiam

---
+++ Arcade-4-Project/Assets/Scripts/Scoreboards/ScoreboardEntryUI.cs
@@ -0,0 +1,17 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace CyberChase.Scoreboards
+{
+    public class ScoreboardEntryUI : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI entryNameText = null;
+        [SerializeField] private TextMeshProUGUI entryScoreText = null;
+
+        public void Initialise(ScoreboardEntryData scoreboardEntryData)
+        {
+            entryNameText.text = scoreboardEntryData.entryName;
+            entryScoreText.text = scoreboardEntryData.entryScore.ToString();
+        }
+    }
+}
